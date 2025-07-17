88f22c107301e34f5fdb87cdb547f52e6bd27062
Denis Yakovlev <clerik1987@mail.ru>
2025-07-17 17:05:53 +0300

    hw04-spring-boot v4

--- hw03-spring-boot/src/test/java/CsvQuestionDaoTest.java
+++ hw03-spring-boot/src/test/java/CsvQuestionDaoTest.java
@@ -1,8 +1,6 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ru.cr.hw.config.TestFileNameProvider;
 import ru.cr.hw.dao.CsvQuestionDao;
