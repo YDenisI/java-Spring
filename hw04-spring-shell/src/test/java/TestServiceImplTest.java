7686a24f38a6e2cdcc3a89229ae0fc5e9d5b54d7
Denis Yakovlev <clerik1987@mail.ru>
2025-07-17 16:26:08 +0300

    hw04-spring-shell v2

--- hw04-spring-shell/src/test/java/TestServiceImplTest.java
+++ hw04-spring-shell/src/test/java/TestServiceImplTest.java
@@ -2,14 +2,23 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import ru.cr.hw.Application;
 import ru.cr.hw.dao.QuestionDao;
 import ru.cr.hw.domain.Answer;
 import ru.cr.hw.domain.Question;
 import ru.cr.hw.domain.Student;
 import ru.cr.hw.domain.TestResult;
-import ru.cr.hw.service.LocalizedIOService;
-import ru.cr.hw.service.TestServiceImpl;
+import ru.cr.hw.service.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,16 +27,48 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest(
+        classes = {/*Application.class, */TestServiceImplTest.TestMocksConfig.class},
+        properties = {
+                "spring.shell.interactive.enabled=false",
+                "spring.shell.command.script.enabled=false",
+                "spring.shell.command.quit.enabled=false",
+                "spring.shell.command.clear.enabled=false"
+        }
+)
 class TestServiceImplTest {
 
-    @Mock
+    @Configuration
+    public static class TestMocksConfig {
+
+        @Bean
+        public LocalizedIOService localizedIOService() {
+            return Mockito.mock(LocalizedIOService.class);
+        }
+
+        @Bean
+        public QuestionDao questionDao() {
+            return Mockito.mock(QuestionDao.class);
+        }
+
+        @Bean
+        public TestService testService(@Autowired @Qualifier("localizedIOService") LocalizedIOService ioService, @Autowired QuestionDao questionDao) {
+            return new TestServiceImpl(ioService, questionDao);
+        }
+        @Bean
+        public LocalizedMessagesService localizedMessagesService() {
+            return Mockito.mock(LocalizedMessagesService.class);
+        }
+    }
+
+    @Autowired
+    @Qualifier("localizedIOService")
     private LocalizedIOService ioService;
 
-    @Mock
+    @Autowired
     private QuestionDao questionDao;
 
-    @InjectMocks
+    @Autowired
     private TestServiceImpl testService;
 
     private final Student student = new Student("Yakovlev", "Denis");
