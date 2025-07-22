5694656ec744f27fddcbc5c525d6a9468558a6e4
Denis Yakovlev <clerik1987@mail.ru>
2025-07-22 11:05:01 +0300

    hw04-spring-shell v1

--- hw04-spring-shell/src/main/java/ru/cr/hw/Application.java
+++ hw04-spring-shell/src/main/java/ru/cr/hw/Application.java
@@ -2,8 +2,6 @@ package ru.cr.hw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import ru.cr.hw.service.TestRunnerService;
 
 @SpringBootApplication
 public class Application {
