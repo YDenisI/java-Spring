8354f6ed1090758dae067520fa01d514664764db
Denis Yakovlev <clerik1987@mail.ru>
2025-10-02 22:24:25 +0300

    hw13-acl

---
+++ hw13-acl/src/main/java/ru/cr/hw/services/GenreService.java
@@ -0,0 +1,12 @@
+package ru.cr.hw.services;
+
+
+import ru.cr.hw.dto.GenreDto;
+import java.util.List;
+
+public interface GenreService {
+
+    List<GenreDto> findAll();
+
+    GenreDto findById(Long id);
+}
